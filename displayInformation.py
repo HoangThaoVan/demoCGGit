80ef60a08aa5b0706d315c531e5994e69796e4c6
thaovan123 <vanht32@gmail.com>
2021-09-22 22:49:30 +0900

    nap bai tap buoi thu 2

---
+++ displayInformation.py
@@ -0,0 +1,6 @@
+print("Ho ten: Hoang Thao Van
+Ngay sinh: 02/12/1994
+Dia chi: Tokyo- Nhat Ban
+Muc dich den vowis khoa hoc: Muon hoc hoi
+ ngon ngu lap trinh Python de xin duowc nhieu cong viec tot hon")
+ 
